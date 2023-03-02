6509e32172e9537e3d674e2a6b5cbd59e08ccde1
Malik  Stewart <malik.y2k3@gmail.com>
2023-03-02 15:21:21 -0500

    regenerating html documents.

--- LSP/src/Html documentation/package-search-index.js
+++ LSP/src/Html documentation/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.howard.edu.lsp.hw4"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.howard.edu.hw1"},{"l":"org.howard.edu.lsp.hw2"},{"l":"org.howard.edu.lsp.hw4"}];updateSearchResults();
